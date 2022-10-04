c6051d50ef06feac625d0616ef3bf89f3fcd70ee
dragoonfly614 <dragoonfly614@icloud.com>
2022-10-04 14:33:06 +0800

    join group finished

--- gar_gui.py
+++ gar_gui.py
@@ -26,18 +26,29 @@ def click(event):
     #event.widget.destroy()
     #btn_1.pack()
 
-def onclick(event,i):
-    print(i)
-    event.widget.grid_forget()
-    print(event.widget.cget('text'))
-    student_name = event.widget.cget('text')
-    global group_index
-    group_buts[group_index].append(tk.Button(frame_1,text = student_name, width=10))
-    group_buts[group_index][-1].pack()
+def onclick(event):
+    global group_edit_flag
+    if group_edit_flag:
+        event.widget.grid_forget()
+        print(event.widget.cget('text'))
+        student_name = event.widget.cget('text')
+        global group_index_choice
+        print(f'group_index_choice={group_index_choice}, i={i}')
+        group_buts[group_index_choice].append(tk.Button(frame_1,text = student_name, width = 10))
+        # place the button in the block(3 rows) of group_name, the 2nd row or 3rd row(1st rowfor group_name), and corresponding column(4 column a row)
+        group_buts[group_index_choice][-1].grid(row = group_index_choice * 3 + (len(group_buts[group_index_choice]) - 1) // 4 + 2, column = (len(group_buts[group_index_choice]) - 1) % 4 + 1)
 
-def group1_choice(event):
-    global group_index
-    group_index = 0
+def group_choice(event, i):
+    global group_edit_flag
+    group_edit_flag = not(group_edit_flag)
+    if group_edit_flag:
+        event.widget.config(bg = 'red',fg = 'white')
+    else:
+        event.widget.config(bg = 'SystemButtonFace',fg = 'black')
+    global group_index_choice
+    #print(f'group_index_choice={group_index_choice}, i={i}')
+    group_index_choice = i
+
 
 def group_bonus_points(event):
     print('DC')
@@ -57,6 +68,8 @@ sheet = my_excel_file[sheets[0]]
 #column = chr(ord('E')+week-1)
 
 name_row = 5
+group_edit_flag = False
+group_index_choice = 0
 
 
 window = tk.Tk()
@@ -65,23 +78,23 @@ frame_2 = tk.Frame(window, relief = tk.RAISED, bd = 2)
 frame_1.grid(row = 1, column = 1)
 frame_2.grid(row = 1, column = 2)
 
-btn_1 = tk.Button(frame_1,text = 'abc', width=10)
-btn_1.bind('<Button-1>',group1_choice)
-btn_1.bind('<Double-Button-1>',group_bonus_points)
-#btn_1.bind('<Button-3>',group_leader)
-btn_1.pack()
-
 buttons = []
+group_name_buts = []
 group_buts = [[] for i in range(7)]
 
-group_index = 0
+for index in range(7):
+    group_name_buts.append(tk.Button(frame_1,text = f'Group{index}', width=10))
+    group_name_buts[index].bind('<Button-1>',lambda event, i = index: group_choice(event, i))
+    group_name_buts[index].bind('<Double-Button-1>',group_bonus_points)
+    #btn_1.bind('<Button-3>',group_leader)
+    group_name_buts[index].grid(row = index*3+1,column = 1)
 
 i = 0
 
 while sheet['C%d'%name_row].value != None:
     student_name = sheet['C%d'%name_row].value
     buttons.append(tk.Button(frame_2,text = student_name, width=10))
-    buttons[i].bind('<Button-1>',lambda event, i=1: onclick(event, i))
+    buttons[i].bind('<Button-1>', onclick)
     buttons[i].grid(row = i//3+1, column =i%3+1)
     #print(f'i={i}, name_row={name_row}, name={student_name}')
     i += 1
