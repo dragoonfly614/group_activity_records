98b17a455ac6f9ecdafaf1db588e08d3514edd13
dragoonfly614 <dragoonfly614@icloud.com>
2022-10-04 21:51:53 +0800

    Write leader flag and group number into the excel file

--- gar_gui.py
+++ gar_gui.py
@@ -17,15 +17,49 @@ Group adding bonus points: Double-click the group name button to display (in ano
 import tkinter as tk
 import openpyxl
 
+# constants
+CLASS_1 = 'points_ele1.xlsx'
+CLASS_2 = 'points_rob1.xlsx'
+CLASS_3 = 'points_rob2.xlsx'
 
+# find the name in the sheet then return row number
 
-def click(event):
-    print(event.widget.cget('text'))
-    #buttons[0].config(text = event.char)
+# write the group number into the corresponding cell w.r.t. name and sheet
+def add_group(name, sheet, group_index, my_excel_file, excel_path):
+    global name2row_dict
+    sheet['E%d'%name2row_dict[name]].value = group_index
+    my_excel_file.save(excel_path)
 
-    #event.widget.destroy()
-    #btn_1.pack()
+def remove_group(name, sheet, my_excel_file, excel_path):
+    global name2row_dict
+    sheet['E%d'%name2row_dict[name]].value = None
+    sheet['F%d'%name2row_dict[name]].value = None
+    my_excel_file.save(excel_path)
 
+def leader_choice(event):
+    global group_edit_flag, name2row_dict
+    name = event.widget.cget('text')
+    leader_flag = sheet['F%d'%name2row_dict[name]].value
+    if group_edit_flag:
+        if leader_flag == None:
+            sheet['F%d'%name2row_dict[name]].value = 1
+            my_excel_file.save(excel_path)
+            event.widget.config(bg = 'green',fg = 'white')
+        else:
+            sheet['F%d'%name2row_dict[name]].value = None
+            my_excel_file.save(excel_path)
+            event.widget.config(bg = 'SystemButtonFace',fg = 'black')
+
+# click action of frame_1 group members
+def group_click(event):
+    global group_edit_flag
+    if group_edit_flag:
+        event.widget.grid_forget()
+        buttons.append(tk.Button(frame_2,text = event.widget.cget('text'), width = 10))
+        buttons[-1].grid(row = (len(buttons)- 1) // 3 + 1, column = (len(buttons) - 1) % 3 + 1)
+        remove_group(event.widget.cget('text'), sheet, my_excel_file, excel_path)
+
+# click action of frame_2 students(without a group)
 def onclick(event):
     global group_edit_flag
     if group_edit_flag:
@@ -33,11 +67,14 @@ def onclick(event):
         print(event.widget.cget('text'))
         student_name = event.widget.cget('text')
         global group_index_choice
-        print(f'group_index_choice={group_index_choice}, i={i}')
         group_buts[group_index_choice].append(tk.Button(frame_1,text = student_name, width = 10))
+        group_buts[group_index_choice][-1].bind('<Button-1>', group_click)
+        group_buts[group_index_choice][-1].bind('<Button-3>', leader_choice)
         # place the button in the block(3 rows) of group_name, the 2nd row or 3rd row(1st rowfor group_name), and corresponding column(4 column a row)
         group_buts[group_index_choice][-1].grid(row = group_index_choice * 3 + (len(group_buts[group_index_choice]) - 1) // 4 + 2, column = (len(group_buts[group_index_choice]) - 1) % 4 + 1)
+        add_group(student_name, sheet, group_index_choice, my_excel_file, excel_path)
 
+# click action of group name buttons
 def group_choice(event, i):
     global group_edit_flag
     group_edit_flag = not(group_edit_flag)
@@ -51,17 +88,23 @@ def group_choice(event, i):
 
 
 def group_bonus_points(event):
-    print('DC')
     pass
 
-def group_leader(event):
-    print('RC')
-    pass
+
 # this part should be separate as an individual module
-my_excel_file = openpyxl.load_workbook('attendence.xlsx')
+class_choice = input('1 for Applied Electronic major class one, 2 for Robot major class one， 3 for Robot major class two: ')
+if class_choice == '1':
+    excel_path = CLASS_1
+elif class_choice == '2':
+    excel_path = CLASS_2
+elif class_choice == '3':
+    excel_path = CLASS_3
+
+my_excel_file = openpyxl.load_workbook(excel_path)
 sheets = my_excel_file.sheetnames
 
 sheet = my_excel_file[sheets[0]]
+name2row_dict = {}
 
 #print(sheets,sheet)
 #week = int(input('周次：'))
@@ -83,21 +126,30 @@ group_name_buts = []
 group_buts = [[] for i in range(7)]
 
 for index in range(7):
-    group_name_buts.append(tk.Button(frame_1,text = f'Group{index}', width=10))
+    group_name_buts.append(tk.Button(frame_1,text = f'Group{index}', width=10, bd = 3))
     group_name_buts[index].bind('<Button-1>',lambda event, i = index: group_choice(event, i))
     group_name_buts[index].bind('<Double-Button-1>',group_bonus_points)
     #btn_1.bind('<Button-3>',group_leader)
     group_name_buts[index].grid(row = index*3+1,column = 1)
 
-i = 0
 
 while sheet['C%d'%name_row].value != None:
     student_name = sheet['C%d'%name_row].value
-    buttons.append(tk.Button(frame_2,text = student_name, width=10))
-    buttons[i].bind('<Button-1>', onclick)
-    buttons[i].grid(row = i//3+1, column =i%3+1)
+    name2row_dict[student_name] = name_row
+    if sheet['E%d'%name_row].value == None:
+        buttons.append(tk.Button(frame_2,text = student_name, width = 10))
+        buttons[-1].bind('<Button-1>', onclick)
+        buttons[-1].grid(row = (len(buttons) - 1) // 3 + 1, column = (len(buttons) - 1) % 3 + 1)
     #print(f'i={i}, name_row={name_row}, name={student_name}')
-    i += 1
+    else:
+        group_number = sheet['E%d'%name_row].value
+        group_buts[group_number].append(tk.Button(frame_1,text = student_name, width = 10))
+        group_buts[group_number][-1].bind('<Button-1>', group_click)
+        group_buts[group_number][-1].bind('<Button-3>', leader_choice)
+        # place the button in the block(3 rows) of group_name, the 2nd row or 3rd row(1st rowfor group_name), and corresponding column(4 column a row)
+        group_buts[group_number][-1].grid(row = group_number * 3 + (len(group_buts[group_number]) - 1) // 4 + 2, column = (len(group_buts[group_number]) - 1) % 4 + 1)
+        if sheet['F%d'%name_row].value == 1:
+            group_buts[group_number][-1].config(bg = 'green',fg = 'white')
     name_row += 1
 
 
